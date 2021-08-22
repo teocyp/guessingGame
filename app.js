af77efbb9aadc175893c2c739f7799a3c5c853f4
teocyp <teocyp1@gmail.com>
2021-08-22 17:28:40 +0300

    Guessing Game Done

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 let maximum = parseInt(prompt('Enter your maximum number!'));
 
 while (!maximum) {
-    prompt('Please enter a valid number');
+    maximum = parseInt(prompt('Please enter a valid number!'));
 }
+
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt('enter your first guess:'));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt('Too high! Enter a new guess:')
+    } else {
+        guess = prompt('Too low! Enter a new guess:')
+    }
+}
+
+console.log(`You got it! It took you ${attempts} guesses..`);
